5954bc9cfd82451bbbedd49faeeacd0ce353c18d
YTKAB0BP <utkabobr@utkacraft.ru>
2021-12-18 19:28:02 +0300

    Improved build system

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -20,34 +20,28 @@ public class BuildVars {
     public static boolean USE_CLOUD_STRINGS = true;
     public static boolean CHECK_UPDATES = true;
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
-    public static int BUILD_VERSION = 2481;
-    public static String BUILD_VERSION_STRING = "8.3.0";
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
-
+    public static int BUILD_VERSION = BuildConfig.VERSION_CODE;
+    public static String BUILD_VERSION_STRING = BuildConfig.VERSION_NAME;
+    public static int APP_ID = BuildConfig.APP_ID;
+    public static String APP_HASH = BuildConfig.APP_HASH;
     public static String SMS_HASH = isStandaloneApp() ? "w0lkcmTZkKh" : (DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT");
-    public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
+    public static String PLAYSTORE_APP_URL = String.format("https://play.google.com/store/apps/details?id=%s", BuildConfig.APPLICATION_ID);
 
     static {
-        if (ApplicationLoader.applicationContext != null) {
-            SharedPreferences sharedPreferences = ApplicationLoader.applicationContext.getSharedPreferences("systemConfig", Context.MODE_PRIVATE);
+        Context ctx = ApplicationLoader.applicationContext;
+        if (ctx != null) {
+            SharedPreferences sharedPreferences = ctx.getSharedPreferences("systemConfig", Context.MODE_PRIVATE);
             LOGS_ENABLED = DEBUG_VERSION || sharedPreferences.getBoolean("logsEnabled", DEBUG_VERSION);
         }
     }
 
-    private static Boolean standaloneApp;
+    @SuppressWarnings("ConstantConditions")
     public static boolean isStandaloneApp() {
-        if (standaloneApp == null) {
-            standaloneApp = ApplicationLoader.applicationContext != null && "org.telegram.messenger.web".equals(ApplicationLoader.applicationContext.getPackageName());
-        }
-        return standaloneApp;
+        return BuildConfig.BUILD_TYPE.equals("standalone");
     }
 
-    private static Boolean betaApp;
+    @SuppressWarnings("ConstantConditions")
     public static boolean isBetaApp() {
-        if (betaApp == null) {
-            betaApp = ApplicationLoader.applicationContext != null && "org.telegram.messenger.beta".equals(ApplicationLoader.applicationContext.getPackageName());
-        }
-        return betaApp;
+        return BuildConfig.BUILD_TYPE.equals("debug");
     }
 }
